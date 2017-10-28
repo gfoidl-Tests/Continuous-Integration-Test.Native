1114abac627028fe7e6cbd9761edadf7755dce3e
Günther Foidl <gue@korporal.at>
2017-10-28 17:56:56 +0200

    source/Calculator/Calculator.vcxproj added

---
+++ source/Calculator/Calculator.cpp
@@ -0,0 +1,6 @@
+#include "Calculator.h"
+//-----------------------------------------------------------------------------
+int Calculator::Add(const int a, const int b)
+{
+    return a + b;
+}
---
+++ source/Calculator/Calculator.h
@@ -0,0 +1,7 @@
+#pragma once
+//-----------------------------------------------------------------------------
+class Calculator
+{
+public:
+    int Add(const int a, const int b);
+};
