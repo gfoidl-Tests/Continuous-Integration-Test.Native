28cfedab7f47c6b65bfd319574fbf8017310b2a8
Günther Foidl <gue@korporal.at>
2017-10-28 20:51:42 +0200

    Test for Issue #1

--- tests/Calculator.Tests/CalculatorTests/Swap.cpp
+++ tests/Calculator.Tests/CalculatorTests/Swap.cpp
@@ -17,5 +17,18 @@ namespace Calculator_Tests_Calculator
             Assert::AreEqual(4, a);
             Assert::AreEqual(3, b);
         };
+        //---------------------------------------------------------------------
+        TEST_METHOD(Huge_numbers___no_overflow)
+        {
+            int a = INT_MAX - 2;
+            int b = INT_MAX;
+
+            Calculator sut;
+
+            sut.Swap(a, b);
+
+            Assert::AreEqual(INT_MAX, a);
+            Assert::AreEqual(INT_MAX - 2, b);
+        }
     };
 }
