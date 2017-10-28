f773a09bcc61cf12ca64904a8a39526cf4097c92
Günther Foidl <gue@korporal.at>
2017-10-28 18:31:22 +0200

    test-project added

---
+++ tests/Calculator.Tests/Calculator.Add.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+
+namespace Calculator_Tests_Calculator
+{
+    TEST_CLASS(Add)
+    {
+    public:
+        TEST_METHOD(Summands_given___correct_result)
+        {
+            int a = 3;
+            int b = 4;
+
+            Calculator sut;
+
+            int actual = sut.Add(a, b);
+
+            Assert::AreEqual(7, actual);
+        };
+    };
+}
